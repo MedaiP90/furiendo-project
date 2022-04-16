75a4269361ca2612fdaa6dcce3b3a88984e7e128
Leonardo Cossutta <leonardo.cossutta@gmail.com>
2022-04-16 16:54:07 +0200

    vercel fixes

--- api/index.js
+++ api/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const http = require('http');
-const apiFuriendo = require('./api/furiendo');
-const fakeDb = require('./tools/fake-db');
+const apiFuriendo = require('./furiendo');
+const fakeDb = require('../tools/fake-db');
 
 /* Configure variables */
 
-const port = process.env.LISTENING_PORT || 3000;
+const port = process.env.LISTENING_PORT || 443;
 
 /* Configure Express */
 
@@ -13,7 +12,11 @@ let app = express();
 
 app.use(express.static('app'));
 app.use(express.json({ limit: '15mb', extended: true }));
-app.set('port', port);
+
+// Run the server
+let server = app.listen(port, () => {
+  console.log('Server is running on port', server.address().port);
+});
 
 /* Configure routing */
 
@@ -23,8 +26,4 @@ app.get('/api/ping', (_req, res) => { res.send('pong'); });
 // Add api responders
 apiFuriendo.add(app, fakeDb, '/api');
 
-/* Create HTTP server */
-
-const server = http.createServer(app);
-
-server.listen(port);
+module.exports = app;
