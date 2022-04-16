6c7744663efcb6d66117bb8695432ef27c0b8802
Leonardo Cossutta <leonardo.cossutta@gmail.com>
2022-04-16 16:45:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const http = require('http');
 const apiFuriendo = require('./api/furiendo');
 const fakeDb = require('./tools/fake-db');
 
 /* Configure variables */
 
-const port = process.env.LISTENING_PORT || 443;
+const port = process.env.LISTENING_PORT || 3000;
 
 /* Configure Express */
 
@@ -12,11 +13,7 @@ let app = express();
 
 app.use(express.static('app'));
 app.use(express.json({ limit: '15mb', extended: true }));
-
-// Run the server
-let server = app.listen(port, () => {
-  console.log('Server is running on port', server.address().port);
-});
+app.set('port', port);
 
 /* Configure routing */
 
@@ -25,3 +22,9 @@ app.get('/api/ping', (_req, res) => { res.send('pong'); });
 
 // Add api responders
 apiFuriendo.add(app, fakeDb, '/api');
+
+/* Create HTTP server */
+
+const server = http.createServer(app);
+
+server.listen(port);
