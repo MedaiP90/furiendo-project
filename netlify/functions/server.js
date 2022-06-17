4ba7b900f131bb496f060030ccd8f0c630daca0b
Leonardo Cossutta <leonardo.cossutta@gmail.com>
2022-06-17 16:25:24 +0200

    Update server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -5,17 +5,20 @@ const fakeDb = require('../../tools/fake-db');
 
 /* Configure Express */
 
-let app = express();
+const app = express();
+const router = express.Router();
 
 app.use(express.json({ limit: '15mb', extended: true }));
 
 /* Configure routing */
 
 // Default ping response
-app.get('/api/ping', (_req, res) => { res.send('pong'); });
+router.get('/api/ping', (_req, res) => { res.send('pong'); });
 
 // Add api responders
-apiFuriendo.add(app, fakeDb, '/api');
+apiFuriendo.add(router, fakeDb, '/api');
+
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
